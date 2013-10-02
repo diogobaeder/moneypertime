3bff94331df4e00f339612b69776eea0a755eefa
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-10-02 12:46:16 -0300

    Performance filter

--- moneypertime/stores/admin.py
+++ moneypertime/stores/admin.py
@@ -7,9 +7,28 @@ def size(instance):
     return u'{}x{}'.format(instance.size1, instance.size2)
 
 
+class PerformanceFilter(admin.SimpleListFilter):
+    title = 'performance'
+    parameter_name = 'performance'
+
+    def lookups(self, request, model_admin):
+        return (
+            (0.5, 'From 0.5'),
+            (1.0, 'From 1.0'),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+
+        if value is None:
+            return
+
+        return queryset.filter(performance__gte=value)
+
+
 class StoreAdmin(admin.ModelAdmin):
     list_display = ['name', 'performance', 'price', 'price_type', 'employees', size, 'build_time', 'amount', 'interval', 'should_create_on_water']
-    list_filter = ['price_type', 'should_create_on_water', 'size1', 'size2']
+    list_filter = ['price_type', 'should_create_on_water', 'size1', 'size2', PerformanceFilter]
 
 
 admin.site.register(Store, StoreAdmin)
